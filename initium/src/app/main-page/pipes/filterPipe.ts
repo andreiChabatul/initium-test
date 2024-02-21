b4e9c40b73ebaeceeecf779220b00ed7a31a1361
Andrei Chabatul <andreichabatul@gmail.com>
2024-02-21 15:34:03 +0300

    feat: add sort and filter pipe

---
+++ initium/src/app/main-page/pipes/filterPipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { itemUser } from '../components/table-main/table-main.component';
+import { userField } from 'src/app/types';
+import { tableStore } from 'src/app/types/store';
+
+@Pipe({
+    name: 'filterPipe'
+})
+export class FilterPipe implements PipeTransform {
+
+    transform(value: itemUser[], tableStore: tableStore | undefined): itemUser[] {
+        if (tableStore && tableStore.filter && tableStore.sortTable) {
+            const filter = tableStore.filter.toLowerCase();
+            const field = tableStore.sortTable?.field;
+            return value.filter((user) => user.value[field].toLowerCase().includes(filter))
+        } else {
+            return value;
+        };
+
+    }
+
+}
---
+++ initium/src/app/main-page/pipes/sortPipe.ts
@@ -0,0 +1,39 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { itemUser } from '../components/table-main/table-main.component';
+import { Store } from '@ngrx/store';
+import { AppStore, sortTable, tableStore } from 'src/app/types/store';
+import { selectTableSort } from 'src/store/selectors';
+import { map } from 'rxjs';
+import { userField } from 'src/app/types';
+
+@Pipe({
+    name: 'sortPipe'
+})
+export class SortPipe implements PipeTransform {
+
+    sortTable$ = this.store.select(selectTableSort);
+
+    constructor(private store: Store<AppStore>) { }
+
+    transform(value: itemUser[], sortTable: sortTable | undefined): itemUser[] {
+
+        if (sortTable) {
+            const type = sortTable.type
+            const field = sortTable.field;
+            return (type === 'ASC')
+                ? this.sortASC(value, field)
+                : this.sortDESC(value, field)
+        } else {
+            return value;
+        };
+
+    }
+
+    sortASC(value: itemUser[], field: userField): itemUser[] {
+        return value.sort((user1, user2) => user2.value[field].localeCompare(user1.value[field]));
+    }
+
+    sortDESC(value: itemUser[], field: userField): itemUser[] {
+        return value.sort((user1, user2) => user1.value[field].localeCompare(user2.value[field]));
+    }
+}
